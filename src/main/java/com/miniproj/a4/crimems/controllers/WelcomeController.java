a37702a0c919bc08b5273d705fbe88a0e32ae045
Divesh2201 <diveshthakker2201@gmail.com>
2020-10-02 11:43:59 +0530

    database integration

--- src/main/java/com/miniproj/a4/crimems/controllers/WelcomeController.java
+++ src/main/java/com/miniproj/a4/crimems/controllers/WelcomeController.java
@@ -1,11 +1,38 @@
 package com.miniproj.a4.crimems.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.miniproj.a4.crimems.entity.Criminal;
+import com.miniproj.a4.crimems.entity.CriminalRepository;
 @Controller
 public class WelcomeController {  
+	
+	@Autowired
+	private CriminalRepository criminalRepository;
 	
     @RequestMapping("/welcome")
     public String loginMessage(){
         return "welcome";
     }
+    @PostMapping(path="/addCriminal") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestParam String first_name
+        , @RequestParam String last_name, @RequestParam String dob,
+        @RequestParam String crime_committed, @RequestParam int prison_term) {
+      // @ResponseBody means the returned String is the response, not a view name
+      // @RequestParam means it is a parameter from the GET or POST request
+
+      Criminal criminal = new Criminal();
+      criminal.setFirst_name(first_name);
+      criminal.setLast_name(last_name);
+      criminal.setCrime_committed(crime_committed);
+      criminal.setDob(dob);
+      criminal.setPrison_term(prison_term);
+      criminalRepository.save(criminal);
+      return "Saved";
+    }
+
 }
---
+++ src/main/java/com/miniproj/a4/crimems/entity/CriminalRepository.java
@@ -0,0 +1,8 @@
+package com.miniproj.a4.crimems.entity;
+import org.springframework.data.repository.CrudRepository;
+
+import com.miniproj.a4.crimems.entity.Criminal;
+
+public interface CriminalRepository extends CrudRepository<Criminal, Integer> {
+
+}
