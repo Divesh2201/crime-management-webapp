7d1cb958b44971f955ac82c2230a10ec4ea1f889
unknown <you@example.com>
2020-10-03 16:16:25 +0530

    Added Missing.java

---
+++ src/main/java/com/miniproj/a4/crimems/entity/MissingRepository.java
@@ -0,0 +1,7 @@
+package com.miniproj.a4.crimems.entity;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface MissingRepository extends CrudRepository<Missing, Integer> {
+
+}
